ba68f95f57f03eece6f192bd10e8ff11dea940bf
Anass Benfillous <anasse.benfillous@gmail.com>
2024-05-12 00:06:59 +0100

    Update 2024_05_09_212430_create_cities_table.php

--- app_server/database/migrations/2024_05_09_212430_create_cities_table.php
+++ app_server/database/migrations/2024_05_09_212430_create_cities_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('cities', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->foreignId('region_id')->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
     }
