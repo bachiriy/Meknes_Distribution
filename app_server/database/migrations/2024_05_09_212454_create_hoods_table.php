4d51ce314f5276036ad9ce0f408892df884755fa
Anass Benfillous <anasse.benfillous@gmail.com>
2024-05-12 00:07:01 +0100

    Update 2024_05_09_212454_create_hoods_table.php

--- app_server/database/migrations/2024_05_09_212454_create_hoods_table.php
+++ app_server/database/migrations/2024_05_09_212454_create_hoods_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('hoods', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->foreignId('city_id')->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
     }
