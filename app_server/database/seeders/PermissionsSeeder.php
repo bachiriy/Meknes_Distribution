33597cb661c149425beae14c9172852c9788f29b
Anass Benfillous <anasse.benfillous@gmail.com>
2024-05-11 18:48:19 +0100

    Update PermissionsSeeder.php

--- app_server/database/seeders/PermissionsSeeder.php
+++ app_server/database/seeders/PermissionsSeeder.php
@@ -11,35 +11,39 @@ class PermissionsSeeder extends Seeder
 {
     private $roles = [
         'admin',
-        'user',
-        'client'
+        'sub-admin'
     ];
 
-    private $permissions = [
-        'view-dashboard',
-        'role-list',
-        'role-create',
-        'role-edit',
-        'role-delete',
-        'feed-create',
-        'feed-delete',
-        'update-user-role',
-        'delete-user',
-        'category-list',
-        'category-create',
-        'category-delete',
-        'news-list',
-        'news-delete',
-        'favorite-list',
-        'favorite-liking',
-        'comment-create',
-        'comment-edit',
-        'comment-delete',
+    private $adminPermissions = [
+        'view dashboard',
+        'update user role',
+        'user list',
+        'add user',
+        'delete user',
+        'client list',
+        'create client',
+        'delete client',
+        'clientFolder list',
+        'create clientFolder',
+        'update clientFolder',
+        'delete clientFolder',
+        'see statistics'
+    ];
+
+    private $subAdminPermissions = [
+        'view dashboard',
+        'client list',
+        'create client',
+        'delete client',
+        'clientFolder list',
+        'create clientFolder',
+        'update clientFolder',
+        'delete clientFolder',
     ];
 
     public function run(): void
     {
-        foreach ($this->permissions as $permission) {
+        foreach ($this->adminPermissions as $permission) {
             Permission::create(['name' => $permission]);
         }
 
@@ -47,7 +51,7 @@ class PermissionsSeeder extends Seeder
             Role::create(['name' => $role]);
         }
 
-        Role::findByName('admin')->givePermissionTo($this->permissions);
-        Role::findByName('user')->givePermissionTo(['favorite-list', 'favorite-liking', 'comment-create', 'comment-edit', 'comment-delete']);
+        Role::findByName('admin')->givePermissionTo($this->adminPermissions);
+        Role::findByName('sub-admin')->givePermissionTo($this->subAdminPermissions);
     }
 }
