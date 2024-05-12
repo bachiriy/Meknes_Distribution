d0b02e371fe5fdb71a0d126a72700cfc604145df
Anass Benfillous <anasse.benfillous@gmail.com>
2024-05-12 01:13:34 +0100

    add client crud method

--- app_server/app/Http/Controllers/ClientController.php
+++ app_server/app/Http/Controllers/ClientController.php
@@ -2,9 +2,62 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Client;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ClientController extends Controller
 {
-    //
+    function index(): \Illuminate\Http\JsonResponse
+    {
+        $clients = Client::all();
+        $response = [
+            'message' => 'success',
+            'clients' => $clients
+        ];
+        return response()->json($response, 200);
+    }
+
+    function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'first_name' => 'required|string',
+            'last_name' => 'required|string',
+            'card_id' => 'required|string',
+            'address' => 'required|string',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        $data = $request->only(
+            'first_name',
+            'last_name',
+            'card_id',
+            'address',
+        );
+
+        $client = Client::create($data);
+        $response = [
+            'status' => 'success',
+            'message' => 'Client is created successfully.',
+            'client' => $client,
+        ];
+        return response()->json($response);
+    }
+
+    function update()
+    {
+
+    }
+
+    function destroy($id): \Illuminate\Http\JsonResponse
+    {
+        $client = Client::find($id);
+        $client->delete();
+        return response()->json([
+            'status' => 'success'
+        ]);
+    }
 }
