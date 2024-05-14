8bbb7895db47ec662dd2f7717ba2eaf26c8db841
Anass Benfillous <anasse.benfillous@gmail.com>
2024-05-14 10:55:41 +0100

    Update DatabaseSeeder.php

--- app_server/database/seeders/DatabaseSeeder.php
+++ app_server/database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             PermissionsSeeder::class,
             AdminSeeder::class,
-            /*CommentSeeder::class,*/
+            CitiesSeeder::class
         ]);
     }
 }
