55eb6a2d5c01a3518afa989e38400150c4f6ce29
Anass Benfillous <anasse.benfillous@gmail.com>
2024-05-14 14:56:08 +0100

    Update Client.php

--- app_server/app/Models/Client.php
+++ app_server/app/Models/Client.php
@@ -10,15 +10,21 @@ class Client extends Model
     use HasFactory;
 
     protected $fillable = [
+        'CIN_ICE',
+        'type',
         'first_name',
         'last_name',
-        'card_id',
-        'address',
+        'email',
+        'phone',
+        'address_exploitation',
+        'suite_address_exploitation',
+        'address_facturation',
+        'suite_address_facturation',
     ];
 
 
-    function clientFiles(): \Illuminate\Database\Eloquent\Relations\HasMany
+    function clientFiles(): \Illuminate\Database\Eloquent\Relations\BelongsToMany
     {
-        return $this->hasMany(ClientFile::class);
+        return $this->belongsToMany(ClientFile::class, 'client_partners');
     }
 }
