430e5d44c11b74de60f7a3c00fe3a9471d9e8884
Anass Benfillous <anasse.benfillous@gmail.com>
2024-05-14 14:56:20 +0100

    Update Product.php

--- app_server/app/Models/Product.php
+++ app_server/app/Models/Product.php
@@ -10,14 +10,33 @@ class Product extends Model
     use HasFactory;
 
     protected $fillable = [
-        'name',
-        'brand',
-        'model',
+        'reference',
+        'designation',
+        'image',
+        'prix_tarif',
+        'prix_achat',
+        'prix_vente',
+        'marge_brut',
+        'remise',
+        'TVA',
+        'prix_vente_net',
+        'category_id',
+        'group_id'
     ];
 
 
     function clientFiles(): \Illuminate\Database\Eloquent\Relations\HasMany
     {
         return $this->hasMany(ClientFile::class);
+    }
+
+    function category(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    function group(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(Group::class);
     }
 }
