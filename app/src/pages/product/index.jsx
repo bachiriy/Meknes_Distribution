26cd00ce6ec30dc7826a23aca2828b4686fe6372
Anass Benfillous <anasse.benfillous@gmail.com>
2024-05-18 14:03:16 +0100

    Update index.jsx

--- app/src/pages/product/index.jsx
+++ app/src/pages/product/index.jsx
@@ -1,47 +1,72 @@
-import { useLayoutEffect, useMemo, useState } from "react";
+import { useEffect, useState } from "react";
 import DataTable from "../../components/DataTable";
 import getProductData from "./getData";
 import Spinner from "../../components/Spinner";
 
+const columns = [
+  {
+    accessorKey: "id",
+    header: "Id",
+    size: 100,
+  },
+  {
+    accessorKey: "designation",
+    header: "Designation",
+    size: 200,
+  },
+  {
+    accessorKey: "supplier.name",
+    header: "Fournisseur",
+    size: 100,
+  },
+  {
+    accessorKey: "group.name",
+    header: "Group",
+    size: 200,
+  },
+  {
+    accessorKey: "group.category.name",
+    header: "Category",
+  },
+  {
+    accessorKey: "marge_brut",
+    header: "Marge Brut",
+  },
+  {
+    accessorKey: "prix_achat",
+    header: "Prix Achat",
+  },
+  {
+    accessorKey: "prix_tarif",
+    header: "Prix Tarif",
+  },
+  {
+    accessorKey: "prix_vente",
+    header: "Prix Vente",
+  },
+  {
+    accessorKey: "prix_vente_net",
+    header: "Prix Vente Net",
+  },
+  {
+    accessorKey: "remise",
+    header: "Remise",
+  },
+  {
+    accessorKey: "reference",
+    header: "Reference",
+  },
+];
 export const Product = () => {
   const [loading, setLoading] = useState(false);
   const [data, setData] = useState(null);
-  
-  const columns = useMemo(
-    () => [
-      {
-        accessorKey: "CIN_ICE", //access nested data with dot notation
-        header: "CIN ICE",
-        size: 150,
-      },
-      {
-        accessorKey: "CIN_file",
-        header: "CIN file",
-        size: 150,
-      },
-      {
-        accessorKey: "address", //normal accessorKey
-        header: "Address",
-        size: 200,
-      },
-      {
-        accessorKey: "first_name",
-        header: "First Name",
-        size: 150,
-      },
-      {
-        accessorKey: "phone",
-        header: "Phone Number",
-        size: 150,
-      },
-    ],
-    []
-  );
-  useLayoutEffect(() => {
+
+  useEffect(() => {
     const waitForIt = async () => {
       setLoading(true);
-      const data = await getProductData();
-      setLoading(false)
+      setData(await getProductData());
+      setLoading(false);
+      console.log(data);
     };
 
     waitForIt();
@@ -50,6 +75,9 @@ export const Product = () => {
   return loading ? (
     <Spinner />
   ) : (
-    <div className="flex">{/* <DataTable /> */}</div>
+    <div className="ml-12 p-4">
+      <h1 className="pb-12 text-center">Products Table</h1>
+      {data && <DataTable data={data} columns={columns} />}
+    </div>
   );
 };
