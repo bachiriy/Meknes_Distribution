6702324d6fbb26cf498ad957d1ea4c5b182d8d14
Anass Benfillous <anasse.benfillous@gmail.com>
2024-05-18 14:03:02 +0100

    Update DataTable.jsx

--- app/src/components/DataTable.jsx
+++ app/src/components/DataTable.jsx
@@ -1,18 +1,14 @@
-import { useMemo } from 'react';
 import {
   MaterialReactTable,
   useMaterialReactTable,
 } from 'material-react-table';
 
 const Table = ({data, columns}) => {
-
-  const memoizedColumns = useMemo(() => columns, [columns]);
-  const memoizedData = useMemo(() => data, [data]);
-
   const table = useMaterialReactTable({
     columns,
-    data, 
+    data
   });
+  
 
   return <MaterialReactTable icons={'hey'} table={table} />;
 };
