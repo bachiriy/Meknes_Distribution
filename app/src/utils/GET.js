996dc204116d316f58e86a1da3e1927569a7a094
mohammed-el-bachiri <med.el.bachiri.00@gmail.com>
2024-05-18 16:21:44 +0100

    create get helper

---
+++ app/src/utils/GET.js
@@ -0,0 +1,21 @@
+import Cookies from "js-cookie";
+
+const API_URL = "http://127.0.0.1:8000/api/"; // this should be in .env file
+
+async function get({endpoint}) {
+    const token = Cookies.get("token");
+    const response = await fetch(API_URL + '/' + endpoint, {
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + token,
+        },
+    });
+
+    if (response.ok) {
+        return await response.json();
+    } else {
+        return 'response not OK';
+    }
+}
+
+export default get;
