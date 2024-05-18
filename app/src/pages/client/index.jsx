399f503e3170ec6346b0e16c8e3541299f1ecd90
mohammed-el-bachiri <med.el.bachiri.00@gmail.com>
2024-05-18 16:22:28 +0100

    fetch clients and display them in main table

--- app/src/pages/client/index.jsx
+++ app/src/pages/client/index.jsx
@@ -1,9 +1,44 @@
-import Table from "../../components/client/Table";
+import ClinetTable from "../../components/DataTable";
+import Spinner from "../../components/Spinner";
+import GET from '../../utils/GET';
+import { useEffect, useState } from "react";
+
+const columns = [
+    { accessorKey: "id", header: "Id", size: 100 },
+    { accessorKey: "designation", header: "Designation", size: 200 },
+    { accessorKey: "supplier.name", header: "Fournisseur", size: 100 },
+    { accessorKey: "group.name", header: "Group", size: 200 },
+    { accessorKey: "group.category.name", header: "Category" },
+    { accessorKey: "marge_brut", header: "Marge Brut" },
+    { accessorKey: "prix_achat", header: "Prix Achat" },
+    { accessorKey: "prix_tarif", header: "Prix Tarif" },
+    { accessorKey: "prix_vente", header: "Prix Vente" },
+    { accessorKey: "prix_vente_net", header: "Prix Vente Net" },
+    { accessorKey: "remise", header: "Remise" },
+    { accessorKey: "reference", header: "Reference" },
+];
+
 
 const Client = () => {
-    return (
+
+    const [loading, setLoading] = useState(false);
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        let recieve = async () => {
+            setLoading(true);
+            setData(await GET('clients'));
+            setLoading(false);
+        };
+        recieve();
+    }, []);
+
+
+
+    return loading ? (<Spinner />) : 
+    (
         <div className="ml-12 p-4">
-        <Table />
+            {data ? (<ClinetTable columns={columns} data={data} />) : (<p>No Data.</p>)}
         </div>
     );
 }
