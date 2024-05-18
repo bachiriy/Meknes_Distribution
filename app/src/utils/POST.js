bea01d31d44cb8d79b1e1e8d6e728a6489152d2f
mohammed-el-bachiri <med.el.bachiri.00@gmail.com>
2024-05-18 17:07:19 +0100

    add POST helper for posting data for mutliple endpoints

--- app/src/utils/POST.js
+++ app/src/utils/POST.js
@@ -2,13 +2,15 @@ import Cookies from "js-cookie";
 
 const API_URL = "http://127.0.0.1:8000/api/"; // this should be in .env file
 
-async function GET(endpoint) {
+async function POST(endpoint, body) { // body : Object
     const token = Cookies.get("token");
     const response = await fetch(API_URL + endpoint, {
         headers: {
             "Content-Type": "application/json",
             Authorization: "Bearer " + token,
         },
+        method: "POST",
+        body: JSON.stringify(body)
     });
 
     if (response.ok) {
@@ -18,4 +20,4 @@ async function GET(endpoint) {
     }
 }
 
-export default GET;
+export default POST;
