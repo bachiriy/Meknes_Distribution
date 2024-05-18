8c2306bb72b29572a6b22cfa4a78fb6a5b3365e4
Anass Benfillous <anasse.benfillous@gmail.com>
2024-05-18 12:41:31 +0100

    Update index.jsx

--- app/src/pages/product/index.jsx
+++ app/src/pages/product/index.jsx
@@ -1,7 +1,55 @@
-import React from 'react'
+import { useLayoutEffect, useMemo, useState } from "react";
+import DataTable from "../../components/DataTable";
+import getProductData from "./getData";
+import Spinner from "../../components/Spinner";
 
 export const Product = () => {
-  return (
-    <div className='text-center'>Product</div>
-  )
-}
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState(null);
+  
+  const columns = useMemo(
+    () => [
+      {
+        accessorKey: "CIN_ICE", //access nested data with dot notation
+        header: "CIN ICE",
+        size: 150,
+      },
+      {
+        accessorKey: "CIN_file",
+        header: "CIN file",
+        size: 150,
+      },
+      {
+        accessorKey: "address", //normal accessorKey
+        header: "Address",
+        size: 200,
+      },
+      {
+        accessorKey: "first_name",
+        header: "First Name",
+        size: 150,
+      },
+      {
+        accessorKey: "phone",
+        header: "Phone Number",
+        size: 150,
+      },
+    ],
+    []
+  );
+  useLayoutEffect(() => {
+    const waitForIt = async () => {
+      setLoading(true);
+      const data = await getProductData();
+      setLoading(false)
+    };
+
+    waitForIt();
+  }, []);
+
+  return loading ? (
+    <Spinner />
+  ) : (
+    <div className="flex">{/* <DataTable /> */}</div>
+  );
+};
