c88c78c8d7b116ccee63551944df117aebd21749
Anass Benfillous <anasse.benfillous@gmail.com>
2024-05-19 13:48:04 +0100

    stote data in session

--- app/src/utils/GET.js
+++ app/src/utils/GET.js
@@ -3,19 +3,25 @@ import Cookies from "js-cookie";
 const API_URL = "http://127.0.0.1:8000/api/"; // this should be in .env file
 
 async function GET(endpoint) {
+  let data = sessionStorage.getItem(endpoint);
+  if (!data) {
     const token = Cookies.get("token");
     const response = await fetch(API_URL + endpoint, {
-        headers: {
-            "Content-Type": "application/json",
-            Authorization: "Bearer " + token,
-        },
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + token,
+      },
     });
 
     if (response.ok) {
-        return await response.json();
+      data = await response.json();
+      sessionStorage.setItem(endpoint, JSON.stringify(data));
+      return data;
     } else {
-        return 'response not OK';
+      return "response not OK";
     }
+  }
+  return JSON.parse(data);
 }
 
 export default GET;
