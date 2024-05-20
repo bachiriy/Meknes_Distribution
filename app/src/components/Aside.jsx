19e77e57a1c94a03dc83ec8e66cd4615fcc5e1a9
Anass Benfillous <anasse.benfillous@gmail.com>
2024-05-20 11:45:00 +0100

    add category icon in aside

--- app/src/components/Aside.jsx
+++ app/src/components/Aside.jsx
@@ -108,6 +108,21 @@ const Aside = ({ page, setPage, setIsConnected }) => {
     {
       pageValue: 6,
       pageLink: "",
+      tooltip: "Categories",
+      icon: (
+        <>
+          <path stroke="none" d="M0 0h24v24H0z" />{" "}
+          <rect x="4" y="4" width="6" height="6" rx="1" />{" "}
+          <rect x="4" y="14" width="6" height="6" rx="1" />{" "}
+          <rect x="14" y="14" width="6" height="6" rx="1" />{" "}
+          <line x1="14" y1="7" x2="20" y2="7" />{" "}
+          <line x1="17" y1="4" x2="17" y2="10" />
+        </>
+      ),
+    },
+    {
+      pageValue: 7,
+      pageLink: "",
       tooltip: "Parametres",
       icon: (
         <>
