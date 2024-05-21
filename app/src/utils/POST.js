de63503a4bf7c7e2536ef83c6a7aafb2914c4884
Anass Benfillous <anasse.benfillous@gmail.com>
2024-05-21 13:21:24 +0100

    update api url

--- app/src/utils/POST.js
+++ app/src/utils/POST.js
@@ -1,6 +1,6 @@
 import Cookies from "js-cookie";
 
-const API_URL = "http://127.0.0.1:8000/api/"; // this should be in .env file
+const API_URL = import.meta.env.VITE_REACT_APP_API_URL;
 
 async function POST(endpoint, body) { // body : Object
     const token = Cookies.get("token");
