daed21d98e2f60c56f7bd2eada2cd6303096e50e
Anass Benfillous <anasse.benfillous@gmail.com>
2024-05-21 13:21:38 +0100

    Create index.jsx

---
+++ app/src/pages/setting/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Profile from '../../components/Setting/Profile'
+
+function Setting() {
+  return (
+    <>
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet" />
+      <p className='text-center'>Setting</p>
+      <Profile />
+
+    </>
+  )
+}
+
+export default Setting
