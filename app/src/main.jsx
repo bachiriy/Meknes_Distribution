0c9914f07613ce2649fa28fb35a224b7a9580bab
mohammed-el-bachiri <med.el.bachiri.00@gmail.com>
2024-05-21 12:16:23 +0100

    fix min, max, close

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -2,6 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
+import { appWindow } from '@tauri-apps/api/window';
+
+let min = document.getElementById('titlebar-minimize');
+min.addEventListener('click', () => appWindow.minimize());
+
+let max = document.getElementById('titlebar-maximize');
+max.addEventListener('click', () => appWindow.toggleMaximize());
+
+let close = document.getElementById('titlebar-close');
+close.addEventListener('click', () => appWindow.close());
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
