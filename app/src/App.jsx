de5ef18464109085abe4449f10ce80b4b818a1b6
Anass Benfillous <anasse.benfillous@gmail.com>
2024-05-21 14:56:47 +0100

    add QueryClientProvider

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -2,15 +2,18 @@ import React, { useState } from "react";
 import { Router } from "./routing/router";
 import "./App.css";
 import Spinner from "./components/Spinner";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   const [loading, setLoading] = useState(true);
 
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       {loading && <Spinner />}
-      <Router setLoading={setLoading}/>
-    </>
+      <Router setLoading={setLoading} />
+    </QueryClientProvider>
   );
 }
 
