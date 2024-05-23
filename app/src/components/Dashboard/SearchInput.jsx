145930c6fa925cc2bcb63668f2f944f22aa9547c
mohammed-el-bachiri <med.el.bachiri.00@gmail.com>
2024-05-23 21:27:44 +0100

    update SearchInput component

--- app/src/components/Dashboard/SearchInput.jsx
+++ app/src/components/Dashboard/SearchInput.jsx
@@ -2,18 +2,17 @@ import * as React from 'react';
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
 
-const SearchInput = ({ options }) => {
-  const [val, setVal] = React.useState();
-  
+const SearchInput = ({ options, onFilter }) => {
+  // onFilter('e', 'v');
+
+
   return (
     <Autocomplete
       disablePortal
       id="combo-box-demo"
       options={options}
       sx={{ width: 800 }}
-      onChange={(e, v) => {
-        setVal(options.includes(v) ? v : null);
-      }}
+      onChange={(e, v) => {onFilter(e, v)}}
       renderInput={(params) => <TextField {...params} label="Les Communes" />}
     />
   );
