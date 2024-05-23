e1adc940e552c0ebd66e5720bc8efea06664f790
mohammed-el-bachiri <med.el.bachiri.00@gmail.com>
2024-05-23 19:44:43 +0100

    handle value state and add onChange callback event

--- app/src/components/Dashboard/SearchInput.jsx
+++ app/src/components/Dashboard/SearchInput.jsx
@@ -2,15 +2,21 @@ import * as React from 'react';
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
 
-const SearchInput = ({options}) => {
+const SearchInput = ({ options }) => {
+  const [val, setVal] = React.useState();
+  
   return (
     <Autocomplete
       disablePortal
       id="combo-box-demo"
       options={options}
       sx={{ width: 800 }}
-      renderInput={(params) => <TextField  {...params} label="Les Communes" />}
+      onChange={(e, v) => {
+        setVal(options.includes(v) ? v : null);
+      }}
+      renderInput={(params) => <TextField {...params} label="Les Communes" />}
     />
   );
-}
+};
+
 export default SearchInput;
