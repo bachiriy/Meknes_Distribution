8ce24bdfb867b48e2346b7a15a07c5a97f82ae1f
mohammed-el-bachiri <med.el.bachiri.00@gmail.com>
2024-05-23 15:57:09 +0100

    update stats icons && show data count corectly

--- app/src/pages/stats/index.jsx
+++ app/src/pages/stats/index.jsx
@@ -7,19 +7,25 @@ import SearchInput from '../../components/Dashboard/SearchInput';
 import { Spinner } from 'flowbite-react';
 
 const icons = {
-  clients: (
+  client: (
     <svg fill="currentColor" viewBox="0 0 20 20" className="w-5 h-5">
       <path d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z"></path>
     </svg>
   ),
-  users: (
-    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 256 256" font-size="var(--icon-fontSize-lg)">
-      <path d="M117.25,157.92a60,60,0,1,0-66.5,0A95.83,95.83,0,0,0,3.53,195.63a8,8,0,1,0,13.4,8.74,80,80,0,0,1,134.14,0,8,8,0,0,0,13.4-8.74A95.83,95.83,0,0,0,117.25,157.92ZM40,108a44,44,0,1,1,44,44A44.05,44.05,0,0,1,40,108Zm210.14,98.7a8,8,0,0,1-11.07-2.33A79.83,79.83,0,0,0,172,168a8,8,0,0,1,0-16,44,44,0,1,0-16.34-84.87,8,8,0,1,1-5.94-14.85,60,60,0,0,1,55.53,105.64,95.83,95.83,0,0,1,47.22,37.71A8,8,0,0,1,250.14,206.7Z"></path>
+  product: (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+      <path stroke-linecap="round" stroke-linejoin="round" d="m21 7.5-9-5.25L3 7.5m18 0-9 5.25m9-5.25v9l-9 5.25M3 7.5l9 5.25M3 7.5v9l9 5.25m0-9v9" />
+    </svg>
+
+  ),
+  clientFile: (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+      <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12.75V12A2.25 2.25 0 0 1 4.5 9.75h15A2.25 2.25 0 0 1 21.75 12v.75m-8.69-6.44-2.12-2.12a1.5 1.5 0 0 0-1.061-.44H4.5A2.25 2.25 0 0 0 2.25 6v12a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9a2.25 2.25 0 0 0-2.25-2.25h-5.379a1.5 1.5 0 0 1-1.06-.44Z" />
     </svg>
   ),
-  files: (
-    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 256 256" font-size="var(--icon-fontSize-lg)">
-      <path d="M80,64a8,8,0,0,1,8-8H216a8,8,0,0,1,0,16H88A8,8,0,0,1,80,64Zm136,56H88a8,8,0,0,0,0,16H216a8,8,0,0,0,0-16Zm0,64H88a8,8,0,0,0,0,16H216a8,8,0,0,0,0-16ZM44,52A12,12,0,1,0,56,64,12,12,0,0,0,44,52Zm0,64a12,12,0,1,0,12,12A12,12,0,0,0,44,116Zm0,64a12,12,0,1,0,12,12A12,12,0,0,0,44,180Z"></path>
+  supplier: (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+      <path stroke-linecap="round" stroke-linejoin="round" d="M15 9h3.75M15 12h3.75M15 15h3.75M4.5 19.5h15a2.25 2.25 0 0 0 2.25-2.25V6.75A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25v10.5A2.25 2.25 0 0 0 4.5 19.5Zm6-10.125a1.875 1.875 0 1 1-3.75 0 1.875 1.875 0 0 1 3.75 0Zm1.294 6.336a6.721 6.721 0 0 1-3.17.789 6.721 6.721 0 0 1-3.168-.789 3.376 3.376 0 0 1 6.338 0Z" />
     </svg>
   )
 };
@@ -40,11 +46,11 @@ const Stats = () => {
 
       let statsSession = JSON.parse(sessionStorage.getItem('stats'));
       if (statsSession && statsSession.message === 'success') {
-        setStats(statsSession.trendingCommune.map((d, i) => ({ ...d, address: statsSession.fullAddresses[i].full_address })));
+        setStats(statsSession);
       } else {
         const response = await GET('stats');
         if (response.message === 'success') {
-          setStats(response.trendingCommune.map((d, i) => ({ ...d, address: response.fullAddresses[i].full_address })));
+          setStats(response);
         }
       }
 
@@ -62,14 +68,19 @@ const Stats = () => {
       </div>
       <div className='w-full grid gap-6 mb-8 md:grid-cols-2 xl:grid-cols-4 mt-6'>
         {statsLoading ? (
-
           <div className='flex justify-center items-center w-screen h-44'>
             <Spinner />
           </div>
         ) :
-          stats.map(i => (
-            <LittNumStat key={i.id} htmlIcon={icons.files} color='blue' txt={i.name} desc={i.address} count={i.client_files_count} />
-          ))
+          (
+            <>
+              <LittNumStat htmlIcon={icons.clientFile} color='yellow' txt="Client Files Count" count={stats.clientFilesCount} />
+              <LittNumStat htmlIcon={icons.client} color='green' txt="Client Count" count={stats.clientsCount} />
+              <LittNumStat htmlIcon={icons.product} color='blue' txt="Products Count" count={stats.productsCount} />
+              <LittNumStat htmlIcon={icons.supplier} color='red' txt="Suppliers Count" count={stats.suppliersCount} />
+
+            </>
+          )
 
         }
       </div>
