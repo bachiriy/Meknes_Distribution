3c76512a30a0f70198f487b6f77742e52254eb5a
Anass Benfillous <anasse.benfillous@gmail.com>
2024-05-24 10:35:51 +0100

    make category name unique

--- app_server/database/migrations/2019_12_14_000221_create_sub_categories_table.php
+++ app_server/database/migrations/2019_12_14_000221_create_sub_categories_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('sub_categories', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->unique();
             $table->unsignedBigInteger('category_id');
             $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
             $table->timestamps();
