637d388113b0ff48423b81e4e675d7074b961a98
Anass Benfillous <anasse.benfillous@gmail.com>
2024-05-29 17:01:31 +0100

    update layout

--- app/src/components/Layout/index.jsx
+++ app/src/components/Layout/index.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Aside from "../../components/Other/Aside";
+import Aside from "../Aside";
 import { Outlet } from "react-router-dom";
 
 export const Layout = (props) => {
