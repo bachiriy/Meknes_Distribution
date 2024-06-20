dffd172e916420922444a8328ac0bbdad8f5dd2b
mohammed-el-bachiri <med.el.bachiri.00@gmail.com>
2024-06-20 16:37:08 +0100

    Update PUT.js

--- app/src/utils/PUT.js
+++ app/src/utils/PUT.js
@@ -3,7 +3,7 @@ import GET from "./GET";
 
 const API_URL = "http://127.0.0.1:8000/api/"; // this should be in .env file
 
-async function PUT(endpoint, id, body=[], updateCach = true) {
+async function PUT(endpoint, id, updateCach = true, body=[]) {
     const token = Cookies.get("token");
     const response = await fetch(`${API_URL + endpoint}/${id}`, {
         headers: {
