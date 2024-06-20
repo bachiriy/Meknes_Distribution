b74033bb6a3a9d64dd224bae377d1ac17dfffb83
mohammed-el-bachiri <med.el.bachiri.00@gmail.com>
2024-06-20 16:36:50 +0100

    Update SubSelect.jsx

--- app/src/components/Client-File/SubSelect.jsx
+++ app/src/components/Client-File/SubSelect.jsx
@@ -7,37 +7,33 @@ import Select from '@mui/material/Select';
 import GET from '../../utils/GET';
 
 export default function SubSelect() {
-
     const [categories, setCategories] = React.useState([]);
 
     React.useEffect(() => {
-        const rtrn = async () => {
-            let r = await GET('categories');
-            if (r.message === 'success') {
-                setCategories(r.categories);
+        const fetchCategories = async () => {
+            let response = await GET('categories');
+            if (response.message === 'success') {
+                setCategories(response.categories);
             }
-        }
-        rtrn();
+        };
+        fetchCategories();
     }, []);
 
     return (
-        <div>
-            <FormControl sx={{ m: 1, minWidth: 120 }}>
-                <InputLabel htmlFor="grouped-native-select">Grouping</InputLabel>
-                <Select native defaultValue="" id="grouped-native-select" label="Grouping">
-                    <option aria-label="None" value="" />
-
-                    {categories.map((i) => (
-                        <optgroup label={i.name}>
-                            {i.sub_categories.map((j)=> (
-                                <option value={j.id}>{j.name}</option>
-                            ))}
-                        </optgroup>
-
-                    ))}
-
-                </Select>
-            </FormControl>
-        </div>
+        <FormControl sx={{ m: 1, minWidth: 120 }}>
+            <InputLabel htmlFor="grouped-native-select">Grouping</InputLabel>
+            <Select native defaultValue="" id="grouped-native-select" label="Grouping">
+                <option aria-label="None" value="" />
+                {categories.map((category) => (
+                    <optgroup key={category.id} label={category.name}>
+                        {category.sub_categories.map((subCategory) => (
+                            <option key={subCategory.id} value={subCategory.id}>
+                                {subCategory.name}
+                            </option>
+                        ))}
+                    </optgroup>
+                ))}
+            </Select>
+        </FormControl>
     );
 }
