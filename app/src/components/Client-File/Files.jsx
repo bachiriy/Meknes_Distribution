a655e9b80f4fa79b8425b78ba2a4d16185f69bc9
Anass Benfillous <anasse.benfillous@gmail.com>
2024-06-23 02:53:29 +0100

    fix side click

--- app/src/components/Client-File/Files.jsx
+++ app/src/components/Client-File/Files.jsx
@@ -23,18 +23,18 @@ const Files = ({ media }) => {
   return (
     <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8 gap-4">
       {media.map((mediaItem) => (
-        <div className="p-3 flex items-center flex-col gap-2 justify-center hover:opacity-80 border border-black hover:shadow-lg transition-all rounded-lg">
-          <Tooltip key={mediaItem.id} content={mediaItem.file_name}>
-            <a href={mediaItem.original_url} target="blank">
+        <a href={mediaItem.original_url} target="blank">
+          <div className="p-3 flex items-center flex-col gap-2 justify-center hover:opacity-80 border border-black hover:shadow-lg transition-all rounded-lg">
+            <Tooltip key={mediaItem.id} content={mediaItem.file_name}>
               <FileIcon mediaItem={mediaItem} />
-            </a>
-          </Tooltip>
-          <div className="w-full text-center">
-            <p className="text-sm truncate overflow-hidden whitespace-nowrap">
-              {mediaItem.file_name}
-            </p>
+            </Tooltip>
+            <div className="w-full text-center">
+              <p className="text-sm truncate overflow-hidden whitespace-nowrap">
+                {mediaItem.file_name}
+              </p>
+            </div>
           </div>
-        </div>
+        </a>
       ))}
     </div>
   );
